1750170657e595cdaf3f9ecd007cd5ecdac7bde0
ctiemer <carl.tiemer@gmail.com>
2020-11-11 11:38:04 +0100

    added deletee

--- public/javascripts/delete.js
+++ public/javascripts/delete.js
@@ -1,3 +1,14 @@
 function deleteFunction() {
-    
+
+        const url = `https://api.airtable.com/v0/appsCMsb32iQNMO3y/Table%201/${query}`;
+
+        fetch(url, { method: 'DELETE', headers: { 'Authorization': 'Bearer keylpPfWBTCbx5mnW', 'Content-Type': 'application/json' } })
+            .then(response => response.json())
+            .then(json => console.log(json))
+            .catch(err => console.log(err));
+
+        setTimeout(function () {
+            location.reload();
+
+        }, 500);
 }
